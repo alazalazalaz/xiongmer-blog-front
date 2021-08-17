41bf6c97d90ecdf04b7bfda90d428233ddd208e7
zhangxiong <zhangxiong@tap4fun.com>
2021-08-17 12:35:31 +0800

    侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,27 @@ module.exports = {
   title: '熊Mer的知识库',
   description: 'golang/mysql/redis/kafka',
 
-  // 导航栏
   themeConfig: {
+    // 导航栏
     nav: [
       {text: 'Home', link: '/'},
       {text: 'Golang', items: [
           {text: '基础知识', link: '/knowledge/golang/base/hello_world.html'},
           {text: '数据结构', link: '/knowledge/golang/base/data_struct.html'}
         ]
+      },
+      {text: 'Mysql', items: [
+          {text: '基础知识', link: '/knowledge/mysql/base/data_type.html'},
+          {text: '数据结构', link: '/knowledge/mysql/data_struct/data_struct.html'}
+        ]
       }
+
+    ],
+
+    //侧边栏
+    sidebar: [
+        ['/knowledge/mysql/base/data_type.md', 'mysql基础知识'],
+        ['/knowledge/mysql/data_struct/data_struct.md', '数据结构']
     ]
   }
 }
