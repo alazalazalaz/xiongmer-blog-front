862d2e80ae74c81e76419033429b9d6b46467b8c
张雄 <1493943799@qq.com>
2021-08-23 00:25:46 +0800

    utf-8

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,43 +14,70 @@ module.exports = {
       {text: 'Mysql', items: [
           {text: '基础知识', link: '/knowledge/mysql/base/data_type.html'},
         ]
+      },
+      {text: 'Linux', items: [
+          {text: '基础知识', link: '/knowledge/linux/base/coding.html'},
+        ]
       }
     ],
 
     //侧边栏
     sidebar: {
-      '/knowledge/golang/': [
-        {
-          title: "Golang基础知识",
-          path: '',
-          collapsable: false,
-          sidebarDepth: 0,
-          children: [
-            ['base/hello_world.md', '基础知识'],
-            ['base/data_struct.md', '数据结构']
-          ]
-        },
-        {
-          title: "Golang进阶知识",
-          path: '',
-          collapsable: false,
-          sidebarDepth: 0,
-          children: [
-            ['base/hello_world.md', 'TODO'],
-          ]
-        }
-      ],
-      '/knowledge/mysql/': [
-        {
-          title: "Mysql基础知识",
-          path: '',
-          collapsable: false,
-          sidebarDepth: 0,
-          children: [
-            ['base/data_type.md', '数据类型'],
-          ]
-        }
-      ]
+      '/knowledge/golang/': getGolangSlider(),
+      '/knowledge/mysql/': getMysqlSlider(),
+      '/knowledge/linux/': getLinuxSlider(),
     }
   }
+}
+
+function getGolangSlider(){
+  return [
+    {
+      title: "Golang基础知识",
+      path: '',
+      collapsable: false,
+      sidebarDepth: 0,
+      children: [
+        ['base/hello_world.md', '基础知识'],
+        ['base/data_struct.md', '数据结构']
+      ]
+    },
+    {
+      title: "Golang进阶知识",
+      path: '',
+      collapsable: false,
+      sidebarDepth: 0,
+      children: [
+        ['base/hello_world.md', 'TODO'],
+      ]
+    }
+  ]
+}
+
+function getMysqlSlider(){
+  return [
+    {
+      title: "Mysql基础知识",
+      path: '',
+      collapsable: false,
+      sidebarDepth: 0,
+      children: [
+        ['base/data_type.md', '数据类型'],
+      ]
+    }
+  ]
+}
+
+function getLinuxSlider(){
+  return [
+    {
+      title: "Linux基础知识",
+      path: '',
+      collapsable: false,
+      sidebarDepth: 0,
+      children: [
+        ['base/coding.md', '编码原理'],
+      ]
+    }
+  ]
 }
